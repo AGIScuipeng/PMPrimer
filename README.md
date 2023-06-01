601fb15967df878cde80863b717bb62d71c6c34a
Nerium <younglon@outlook.com>
2023-06-01 15:38:08 +0800

    工程化调整/ Engine Adjust

--- README.md
+++ README.md
@@ -12,30 +12,38 @@
 + Amplicon design and evaluate
 + Debug mode
 
-## 0x1 
+## 0x1 Installation
+1. Install by code
+   Download source code from this repo, and install primer3-py, biopython, seaborn in your devices, then you can run this software by use `python3 pmprimer.py`
+
+2. Install by pip
+   Install this software by use command `pip(or pip3 for python3) install PMPrimer`.
+   If u use pip to install this software, pip will automatically install dependency packages and add a command pmprimer in your system.
+
+## 0x2 Usage
 1. Input file
    > --file -f
 
-    Use like : -f seqs.fasta or --file seqs.fasta
+    Use like : `pmprimer -f seqs.fasta or --file seqs.fasta`
 
 2. Basic process for data
    > --progress -p
 
     All parameter as follows :
-    + notlen Do not process according to length;
-    + notsameseq Do not process according to same sequences;
+    + notlen Do not processing sequences according to distribution of length
+    + notsameseq Do not remove redanduncy sequences according to three levels
     + matrix Draw heatmap according to mismatch analysis.
 
     >If length distribution is good, should use 'notlen' prevent removal of minority sequences.
 
-    Add parameters behind -p, use like : -p notlen notsameseq matrix, order doesn't matter.
+    Add parameters behind -p, use like : `-p notlen notsameseq matrix`, order doesn't matter.
 
 3. Primer design and extract of multiple alignment
    >--alldesign -a
 
     All parameter as follows :
-    + threshold Threshold to detect conservative regions, default is 0.95, use like : threshold:0.95
-    + minlen Minimum continues length when detect conservative regions, default is 15bp, use like : minlen:15
+    + threshold Threshold for identify conservative region, default is 0.95, use like : threshold:0.95
+    + minlen Minimum continues length when identify conservative regions, default is 15bp, use like : minlen:15
     + gaps Maximum rate of gaps, default is 0.1, use like : gaps:1.0
     + tm Default TM, default is 50, use like : tm:50.0
     + muscle Use MUSCLE to align sequences and save as another file
@@ -43,7 +51,7 @@
     + primer2 Primer design and extract
     + pdetail2 Information of primer2
 
-    add parameters behind -a, like : -a threshold:0.96 minlen:16 merge primer2, order doesn't matter.
+    add parameters behind -a, like : `-a threshold:0.96 minlen:16 merge primer2`, order doesn't matter.
 
 4. Amplicon design and evaluate
    > --evaluate -e
@@ -56,7 +64,7 @@
     + rmlow Remove TM lower than configure in Design module
     + save Save final results
 
-    add parameters behind -e, like : -e hpcnt:50 maxlen:1200 save, order doesn't matter.
+    add parameters behind -e, like : `-e hpcnt:50 maxlen:1200 save`, order doesn't matter.
 
 5. Debug mode
    > --debuglevel -d
@@ -66,19 +74,37 @@
     + 2 Mocule debug info
     + 4 Complex debug info
 
-    If need debug info, use '-d 1' in normal conditions, this number use binary calculate.
+    If need debug info, use `-d 1` in normal conditions, this number use binary calculate.
 
-## 0x2 Tips
+## 0x3 Tips
 1. If input file is alignments, use like :
 
-    pmprimer -f seqs.fasta -a merge primer2 -e hpcnt:70 save
+    `pmprimer -f seqs.fasta -a merge primer2 -e hpcnt:70 save`
 
 2. If input file need data process, use like :
 
-    pmprimer -f seqs_need_filt.fasta -p default
+    `pmprimer -f seqs_need_filt.fasta -p default`
     > this command will process data according to major length and different third level when same sequences.
 
 3. If input file need align, use like : 
 
-    pmprimer -f seqs_need_align.fasta -a muscle merge primer2 -e hpcnt:70 save
+    `pmprimer -f seqs_need_align.fasta -a muscle merge primer2 -e hpcnt:70 save`
     > this command will save alignments as 'seqs_need_align.mc.fasta'
+
+## 0x4 Datasets In Paper
+
+Dataset in paper can obtained by https://github.com/AGIScuipeng/PMPrimer_datasets
+
+1. 16S ribosomal RNA (rRNA) genes of Archaea
+   
+   Archaea_16SrRNA.rep.mc.fasta is more than 200MB, so only upload original file to github, need use use MUSCLE5 to align sequences and save output file as `Archaea_16SrRNA.rep.mc.fasta` or use `pmprimer -f Archaea_16SrRNA.rep.fasta -p notlen notsameseq muscle` to align sequences.
+   Command in paper is : `pmprimer -f Archaea_16SrRNA.rep.mc.fasta -a threshold:0.85 gaps:1.0 merge primer2 haplo tm:45.0 -e hpcnt:600 save`
+
+> 
+2. hsp65 (groEL2) genes of Mycobacteriaceae
+   
+   Command in paper is : `pmprimer -f Mycobacteriaceae_groEL2.filt.mc.fasta -a primer2 -e hpcnt:70 save`
+> 
+3. tuf genes of Staphylococci
+   
+   Command in paper is : `pmprimer -f Staphylococcus_tuf.filt.mc.fasta -a threshold:0.995 minlen:5 merge primer2 -e save`
--- piece/piecentry.py
+++ piece/piecentry.py
@@ -12,7 +12,7 @@ from .piecemain import piecemain
 
 from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter
-import sys
+import sys, locale
 
 '''
 创建人员: Nerium
@@ -24,35 +24,60 @@ import sys
 def entry() :
     #命令行程序
     paramparse = ArgumentParser(description='primer piece', epilog = __version__,formatter_class=RawTextHelpFormatter)
-    paramparse.add_argument('--file', '-f', help='初始序列文件')
-    paramparse.add_argument('--progress', '-p', nargs='+', 
-                            help='''基础数据清洗，参数有：\
-                            \nnotlen 不根据长度分布清洗\
-                            \nnotsameseq 不根据相同序列合并\
-                            \nmatrix 根据序列相似度画出聚类热图''')
-    paramparse.add_argument('--alldesign', '-a', nargs='+',
-                            help='''多序列引物设计，参数有：\
-                            \nthreshold 保守区间判定的阈值，默认为0.95，使用： threshold:0.95\
-                            \nminlen 保守区间连续长度最小值，默认为15bp，使用： minlen:15\
-                            \ngaps 空白符占比最高比例，默认为0.1，使用： gaps:0.1\
-                            \ntm 熔解温度默认值，默认为50，使用： tm:50.0\
-                            \nmuscle 使用muscle进行多序列比对并另存文件\
-                            \nmerge 保守区间合并\
-                            \nprimer 第一次引物设计\
-                            \npdetail 第一次引物设计详细信息输出\
-                            \nprimer2 第二次引物提取\
-                            \npdetail2 第二次引物提取详细信息输出''')
-    paramparse.add_argument('--debuglevel', '-d', help='调试等级', type=int, default=0)
-    paramparse.add_argument('--evaluate', '-e', nargs='+', default=['default'], 
-                            help='''引物标准评估，参数有：\
-                            \nhpcnt HaploType最大值，默认为10，使用： hpcnt:10\
-                            \nminlen 扩增子区间最小值，默认为150，使用： minlen:150\
-                            \nmaxlen 扩增子区间最大值，默认为1500，使用： maxlen:1500\
-                            \nblast 使用文件建库和查询，多个fasta文件使用,来分隔，使用： blast:../taxo1.fasta,homo.fasta\
-                            \nmerge 多个扩增子合并\
-                            \nrmlow 去除引物提取熔解温度低于设计模块配置值的引物\
-                            \nsave 保存所有中间文件''')
-    #paramparse.add_argument('--nextuse', '-n', help='后续SNP挖掘和物种鉴定')
+    if 'zh_CN' == locale.getdefaultlocale()[0] : 
+        paramparse.add_argument('--file', '-f', help='初始序列文件')
+        paramparse.add_argument('--progress', '-p', nargs='+', 
+                                help='''基础数据清洗，参数有：\
+                                \nnotlen 不根据长度分布清洗\
+                                \nnotsameseq 不根据相同序列合并\
+                                \nmatrix 根据序列差异度画出聚类热图''')
+        paramparse.add_argument('--alldesign', '-a', nargs='+',
+                                help='''多序列引物设计，参数有：\
+                                \nthreshold 保守区间判定的阈值，默认为0.95，使用： threshold:0.95\
+                                \nminlen 保守区间连续长度最小值，默认为15bp，使用： minlen:15\
+                                \ngaps 空白符占比最高比例，默认为0.1，使用： gaps:0.1\
+                                \ntm 熔解温度默认值，默认为50，使用： tm:50.0\
+                                \nmuscle 使用muscle进行多序列比对并另存文件\
+                                \nmerge 保守区间合并\
+                                \nprimer2 第二次引物提取\
+                                \npdetail2 第二次引物提取详细信息输出''')
+        paramparse.add_argument('--debuglevel', '-d', help='调试等级', type=int, default=0)
+        paramparse.add_argument('--evaluate', '-e', nargs='+', default=['default'], 
+                                help='''引物标准评估，参数有：\
+                                \nhpcnt HaploType最大值，默认为10，使用： hpcnt:10\
+                                \nminlen 扩增子区间最小值，默认为150，使用： minlen:150\
+                                \nmaxlen 扩增子区间最大值，默认为1500，使用： maxlen:1500\
+                                \nblast 使用文件建库和查询，多个fasta文件使用,来分隔，使用： blast:../taxo1.fasta,homo.fasta\
+                                \nmerge 多个扩增子合并\
+                                \nrmlow 去除引物提取熔解温度低于设计模块配置值的引物\
+                                \nsave 保存所有中间文件''')
+        #paramparse.add_argument('--nextuse', '-n', help='后续SNP挖掘和物种鉴定')
+    else :
+        paramparse.add_argument('--file', '-f', help='Input FASTA file')
+        paramparse.add_argument('--progress', '-p', nargs='+', 
+                                help='''Basic data processing module, has the following parameters: \
+                                \nnotlen Do not processing sequences according to distribution of length\
+                                \nnotsameseq Do not remove redanduncy sequences according to three levels\
+                                \nmatrix Draw heatmap according to mismatch analysis''')
+        paramparse.add_argument('--alldesign', '-a', nargs='+',
+                                help='''Primer design for multiple sequences module, has the following parameters: \
+                                \nthreshold Threshold for identify conservative region, default is 0.95, use like threshold:0.95\
+                                \nminlen Minimum continues length when identify conservative region, default is 15, use like minlen:15\
+                                \ngaps Maximum rate of gaps, default is 0.1, use like : gaps:1.0\
+                                \ntm Default TM, default is 50, use like : tm:50.0\
+                                \nmuscle Use MUSCLE to align sequences and save as another file\
+                                \nmerge Use merge module to merge conservative regions\
+                                \nprimer2 Primer design and extract\
+                                \npdetail2 Information of primer2''')
+        paramparse.add_argument('--debuglevel', '-d', help='调试等级', type=int, default=0)
+        paramparse.add_argument('--evaluate', '-e', nargs='+', default=['default'], 
+                                help='''Amplicon selection and evaluation module, has the following parameters: \
+                                \nhpcnt Maximum count of haploType, default is 10, use like : hpcnt:10\
+                                \nminlen Minimum length of amplicon, default is 150bp, use like : minlen:150\
+                                \nmaxlen Maximum length of amplicon, default is 1500bp, use like : maxlen:1500\
+                                \nblast Use fasta to create blast db and search, use ',' split different files, use like : blast:../taxo1.fasta,homo.fasta\
+                                \nrmlow Remove TM lower than configure in Design module\
+                                \nsave Save final results''')
 
     #解析命令行
     args = paramparse.parse_args()
